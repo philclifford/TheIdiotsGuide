16d6514f284f8f2a1f571817af80ef178f87c422
Phil Clifford <philip.clifford@gmail.com>
2022-07-22 13:13:53 +0100

    small edits

--- hello_world.sh
+++ hello_world.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/bin/env bash
 
 #  This is a stupid example
 
-echo "Hello World"
-echo;echo
+function say_hello() {
+cat <<EOT
+Hello World
+
+
+EOT
+}
+
+say_hello
+echo " .... bye" && \
+exit 0
